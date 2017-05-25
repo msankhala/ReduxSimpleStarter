342cbc205577b2b804a854aa526d770391d0408a
Mahesh Sankhala <maheshsainni@gmail.com>
2017-05-25 19:05:49 +1000

    Revert "working youtbue search app completed"
    This reverts commit b16660b38bc55f136e8aa23de6c90f2d569b7fec.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,15 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import _ from 'lodash'
-import SearchBar from './components/search_bar';
-import VideoDetail from './components/video_detail';
-import VideoList from './components/video_list';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = 'AIzaSyAZIqQa88wUPX0OZUPql4WxKdVhu7_M3tw';
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-    };
-    this.videoSearch('surfboard');
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, (videos) => {
-      this.setState({
-        videos,
-        selectedVideo: videos[0]
-      });
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 500);
-    return (
-      <div>
-        <SearchBar onSearchTermChange={term => videoSearch(term)} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDom.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
